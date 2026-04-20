8df9b3f15c0de9cd4a9f9aac54e0906bed75abd2
AlexanderKosenko <kosya54@gmail.com>
2026-04-21 01:19:18 +0700

    Добавлены простые фильтры по логам

--- bash_scripts/filters.bash
+++ bash_scripts/filters.bash
@@ -2,6 +2,6 @@
 ( echo "COUNT IP_ADDRESS"; sudo journalctl --since "24 hour ago" -t sshd \
     | grep -i "failed password" | awk '{print $(NF-3)}' | sort | uniq -c | sort -rn ) | column -t
 
-#Вывод то 10 процессов по info за 12 часов
-( echo "COUNT PROC"; sudo journalctl --since "12 hour ago" --no-pager -p info \
+#Вывод топ 10 процессов по уровням до warning за 12 часов
+( echo "COUNT PROC"; sudo journalctl --since "12 hour ago" --no-pager -p warning \
     | awk '{print $5}' | cut -d'[' -f1 | sort | uniq -c | sort -rn | head -n 10 ) | column -t
